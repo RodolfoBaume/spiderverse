7dbf0c6d0775af051dae5005119f0898b13805c7
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 17:52:03 -0500

    Se construye la clase con sus atributos

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require('./../app/spiderman')
+
 describe("Unit Test for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
         // Aquí escribimos el código que queremos usar tal cuál 
