8f743c864b1bcb7aa68aca89d64bfdfa73b69712
RodolfoBaume <robalaz@hotmail.com>
2022-04-20 17:57:59 -0500

    Se agrego método getInfo

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -13,4 +13,8 @@ describe("Unit Test for Spiderman class", () => {
         expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
     });
+    test('2) Use the method getInfo()', () => {
+        const tomHolland = new Spiderman("Spidermna Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+    });
   })
